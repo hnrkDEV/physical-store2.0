75e8572c204073fabea77c32b3da1479b9be9397
João Henrique <jhenrique.caval@hotmail.com>
2025-04-08 01:51:03 -0300

    <feat>: implementing ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,18 @@ dotenv.config({ path: 'config.env' });
 
 import { NestFactory } from '@nestjs/core';
 import { StoresModule } from './stores/stores.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(StoresModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  
   await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
