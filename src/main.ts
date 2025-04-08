942ec0bf36cef3b0d4b491713ad4817b8ddd1761
João Henrique <jhenrique.caval@hotmail.com>
2025-04-07 23:30:11 -0300

    <feat>: importing dotenv and setting path

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import * as dotenv from 'dotenv';
+dotenv.config({ path: 'config.env' });
+
 import { NestFactory } from '@nestjs/core';
 import { StoresModule } from './stores/stores.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(StoresModule);
-  await app.listen(process.env.PORT ?? 8000);
+  await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
