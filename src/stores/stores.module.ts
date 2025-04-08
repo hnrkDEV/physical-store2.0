eff4a3d794ad5b8794abeaa457992a1fd3e48beb
João Henrique <jhenrique.caval@hotmail.com>
2025-04-08 00:31:58 -0300

    <feat>: importing logger and stores repository

--- src/stores/stores.module.ts
+++ src/stores/stores.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from '../log/logger.module';
 import { StoresService } from './services/stores.service';
 import { StoresController } from './controllers/stores.controller';
+import { StoresRepository } from './repositories/stores.repository';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Store, StoreSchema } from './schemas/store.schema';
 
+
 @Module({
   imports: [
+  LoggerModule,
   MongooseModule.forRoot(process.env.MONGO_URI!),
   MongooseModule.forFeature([{ name: Store.name, schema: StoreSchema }])
 ],
-  providers: [StoresService],
+  providers: [
+    StoresService,
+    StoresRepository
+  ],
   controllers: [StoresController]
 })
 export class StoresModule {}
