4391ff80f3b3ea1142f9786b4e245f9318263004
João Henrique <jhenrique.caval@hotmail.com>
2025-04-07 23:30:37 -0300

    <feat>: importing config module and mongoose module and setting it up

--- src/stores/stores.module.ts
+++ src/stores/stores.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { StoresService } from './services/stores.service';
 import { StoresController } from './controllers/stores.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Store, StoreSchema } from './schemas/store.schema';
 
 @Module({
+  imports: [
+  MongooseModule.forRoot(process.env.MONGO_URI!),
+  MongooseModule.forFeature([{ name: Store.name, schema: StoreSchema }])
+],
   providers: [StoresService],
   controllers: [StoresController]
 })
