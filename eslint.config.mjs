6319db49c3742f9b9086379650b4005d0f4a4c14
João Henrique <jhenrique.caval@hotmail.com>
2025-04-07 21:15:49 -0300

    initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-// @ts-check
+/* // @ts-check
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
@@ -32,4 +32,4 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-argument': 'warn'
     },
   },
-);
+); */
