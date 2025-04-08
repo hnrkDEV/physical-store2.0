80fa01703c19ac834c1779f49da2d36f7407e1d2
João Henrique <jhenrique.caval@hotmail.com>
2025-04-08 00:34:53 -0300

    <feat>: implementing getAllStores w logs

--- src/stores/services/stores.service.ts
+++ src/stores/services/stores.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, Logger } from '@nestjs/common';
+import { StoresRepository } from '../repositories/stores.repository';
+import { Store } from '../interfaces/store.interface';
 @Injectable()
-export class StoresService {}
+export class StoresService {
+  private readonly logger = new Logger(StoresService.name);
+
+  constructor(private readonly storesRepository: StoresRepository) {}
+
+  async getAllStores(): Promise<Store[]> {
+    this.logger.log('Searching for stores in DB');
+    const stores = await this.storesRepository.findAll();
+    this.logger.log(`${stores.length} Stores`);
+    return stores;
+  }
+}
