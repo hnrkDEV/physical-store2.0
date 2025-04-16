b59165af730391b121d1a9cb04bf426b1d7254df
João Henrique <jhenrique.caval@hotmail.com>
2025-04-16 04:16:26 -0300

    <fix>: dealing better with errors

--- src/geo/via-cep/via-cep.service.ts
+++ src/geo/via-cep/via-cep.service.ts
@@ -1,15 +1,26 @@
-import { Injectable, HttpException } from '@nestjs/common';
+import { Injectable, HttpException, Inject } from '@nestjs/common';
 import axios from 'axios';
-
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
 @Injectable()
 export class ViaCepService {
+  constructor(
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {}
+
   async getAddressByCep(cep: string) {
     try {
       const { data } = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
-      if (data.erro) throw new Error('CEP not found');
+
+      if (data.erro) {
+        this.logger.warn(`CEP not found: ${cep}`);
+        throw new HttpException('ZIP code not found', 404);
+      }
+
       return data;
     } catch (error) {
-      throw new HttpException('Error searching for zip code', 400);
+      this.logger.error(`Error fetching data from ViaCEP: ${error.message}`);
+      throw new HttpException('Failed to retrieve ZIP code information from ViaCEP', 500);
     }
   }
 }
